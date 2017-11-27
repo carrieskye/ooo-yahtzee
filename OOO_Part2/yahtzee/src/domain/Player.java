b3b4ef1d736d86ddd603b5758c34142b74d325ed
Carolyne Peelman <peelmancarolyne@gmail.com>
2017-11-27 13:39:15 +0100

    Gamescreen added

--- OOO_Part2/yahtzee/src/domain/Player.java
+++ OOO_Part2/yahtzee/src/domain/Player.java
@@ -1,6 +1,22 @@
 package domain;
 
-public class Player {
+import java.util.Observable;
+import java.util.Observer;
+
+import view.GameScreen;
+
+public class Player implements Observer{
 	private String username;
+	private GameScreen veld;
+	
+	public Player(String username){
+		veld = new GameScreen();
+	}
+
+	@Override
+	public void update(Observable o, Object arg) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
