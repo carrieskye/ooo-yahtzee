dfd62dd8d1196b29dd959afb01089a82d437b80f
Philip Audenaert <philip.audenaert@student.ucll.be>
2017-11-27 14:37:31 +0100

    Added DomainException + Implemented Game

--- OOO_Part2/yahtzee/src/application/Main.java
+++ OOO_Part2/yahtzee/src/application/Main.java
@@ -1,26 +1,39 @@
 package application;
-	
-import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			Scene scene = new Scene(root, 400, 400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			Button launchYahtzee = new Button("launch Yahtzee");
+			root.setCenter(launchYahtzee);
+			primaryStage.setTitle("GameSuite");
 			primaryStage.setScene(scene);
 			primaryStage.show();
-		} catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
+
 	}
+
+	class launchYahtzeeHandler implements EventHandler<ActionEvent> {
+		@Override
+		public void handle(ActionEvent event) {
+		}
+	}
+
 }
--- OOO_Part2/yahtzee/src/domain/Player.java
+++ OOO_Part2/yahtzee/src/domain/Player.java
@@ -5,18 +5,35 @@ import java.util.Observer;
 
 import view.GameScreen;
 
-public class Player implements Observer{
+public class Player implements Observer {
 	private String username;
 	private GameScreen veld;
-	
-	public Player(String username){
+
+	public Player(String username) throws DomainException {
+		this.setUsername(username);
 		veld = new GameScreen();
 	}
 
 	@Override
 	public void update(Observable o, Object arg) {
 		// TODO Auto-generated method stub
-		
+
+	}
+
+	public void setUsername(String username) throws DomainException {
+		if (username.equals(null) || username.trim().isEmpty()) {
+			throw new DomainException("Username cannot be empty.");
+		} else {
+			this.username = username;
+		}
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public GameScreen getGameScreen() {
+		return veld;
 	}
 
 }
