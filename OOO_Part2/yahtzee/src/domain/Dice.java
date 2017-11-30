ec2192f02db3fd3a36efa0354aab59e36044bb38
Carolyne Peelman <peelmancarolyne@gmail.com>
2017-11-30 22:36:29 +0100

    Dice enum met images in verwerkt

--- OOO_Part2/yahtzee/src/domain/Dice.java
+++ OOO_Part2/yahtzee/src/domain/Dice.java
@@ -1,18 +1,27 @@
 package domain;
 
-import java.util.Random;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import javafx.scene.image.Image;
 
-public class Dice {
-	private int value;
-	
-	public Dice(){
-		value = 0;
+public enum Dice {
+
+	ONE(1), TWO(2), THREE(3), FOUR(4), FIVE(5), SIX(6);
+	private Image img;
+
+	private Dice(int number) {
+		FileInputStream inputstream;
+		try {
+			inputstream = new FileInputStream("resources/dice/Dice-" + number + ".png");
+			this.img = new Image(inputstream);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
 	}
-	
-	public void throwDice(){
-		Random rand = new Random();
-		this.value = rand.nextInt((6 - 1) + 1) + 1;
-		System.out.println(this.value);
+
+	public Image getImage() {
+		return this.img;
 	}
 
 }
